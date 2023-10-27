4d56f843add355fb103f6f6cf47ae847061ddb4d
Andrii Sukhenko <andrii.suhenko@gmail.com>
2023-10-27 11:37:31 +0300

    state component

--- src/index.js
+++ src/index.js
@@ -3,18 +3,33 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Clock(props) {
-	return (
-		<div>
-			<h1>Hello world!</h1>
-			<h2>It is {props.date.toLocaleTimeString()}</h2>
-		</div>
-	)
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { date: new Date() }
+	}
+
+	tick() {
+		this.setState({ date: new Date() });
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(() => this.tick(), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello world!</h1>
+				<h2>It is {this.state.date.toLocaleTimeString()}</h2>
+			</div>
+		)
+	}
 }
 
 
-function tick() {
-	root.render(<Clock date={new Date()}/>);
-}
-
-setInterval(tick, 1000);
+root.render(<Clock/>);
